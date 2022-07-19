6bfa298b77e7afe7dd513170fbe3fec15f76ed0d
hzlnutspread <smithkg293@gmail.com>
2022-07-19 20:02:06 +1200

    Add dictionary for handling DataFrames

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -5,6 +6,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime, timedelta
 from csv import writer
+import pandas as pd
 import time
 
 if datetime.now().strftime("%A") == "Monday":
@@ -43,18 +45,38 @@ def get_data_array(driver):
     values = table.find_all('td')
     for value in values:
         if datetime.now().strftime("%A") == "Monday":
-            date_val = (datetime.now() - timedelta(3)).strftime("%d-%b-%y")
+            date_val = (datetime.now() - timedelta(3)).strftime("%d-%b-%y")                 # format date for csv export
         else:
-            date_val = (datetime.now() - timedelta(1)).strftime("%d-%b-%y")
+            date_val = (datetime.now() - timedelta(1)).strftime("%d-%b-%y")                 # format date for csv export
 
-        print(value.text)
+        subarray = [date_val, value.text]                                                   # create subarrays to append to array
+        array.append(subarray)
 
-            # price = int(value.text)
-            # subarray = [date_val, price]
-            # array.append(subarray)
-    return array
+    fra_data = [array[9], array[14], array[19], array[24], array[29], array[34]]            # data points required
+    print(fra_data)
 
-# trying to figure out how to turn the string numbers into floats to format them to two decimal places.
+    dataframe_collection = {}                                                               # create dictionary
+
+    for i in range(0, 6):
+        df_data = [fra_data[i]]
+        data_frame = pd.DataFrame(df_data, columns=['Date', 'rate'])
+        data_frame['rate'] = data_frame['rate'].astype(float).round(decimals=2)
+        dataframe_collection[f'df{i + 1}'] = data_frame                                     # loop to create a dataframe for each data point
+
+#-------------------------------------------------------------- # return dataframe_collection here to pass into new method to append csvs. Get rid of write_to_csv method
+    for key in dataframe_collection.keys():
+        print("\n" + "=" * 20)
+        print(key)
+        print("-" * 20)
+        print(dataframe_collection[key])                                                    # print it out to make sure its working
+
+    for i in range(0, 6):
+        path = f"C:\\Users\\User\\Desktop\\Book{i + 1}.csv"
+        dataframe_collection[f'df{i + 1}'].to_csv(path, mode='a', index=False, header=False)
+        print("Successfully added")                                                         # append each dataframe onto corresponding csv file
+
+    return dataframe_collection                                                             #
+
 
 def write_to_csv(array):
     print(DATE + ": ")
@@ -62,7 +84,7 @@ def write_to_csv(array):
 
     for x in range(1, 7):
         # location = f'\\SERVER\\jdjl\\interest-nz\\interest.co.nz\\chart_data\\interestrates\\bkbm-{x}mth.csv'
-        file_location = f'C:\\Users\\ken\\Desktop\\Book{x}.csv'
+        file_location = f'C:\\Users\\User\\Desktop\\Book{x}.csv'
         with open(file_location, 'a', newline='') as target_csv:
             writer_object = writer(target_csv)
             if x == 1:
@@ -107,13 +129,13 @@ def ftp_files():
 
 
 if __name__ == '__main__':
-    driver = launch_website()               # launch bkbm website
-    click_calendar_button(driver)           # select calendar
-    select_date(driver)                     # choose date
-    click_search_button(driver)             # click search button
-    time.sleep(1)
-    data_array = get_data_array(driver)     # create the array of data
-    time.sleep(1)
 
-    # write_to_csv(data_array)                # update csv files
+    driver = launch_website()  # launch bkbm website
+    click_calendar_button(driver)  # select calendar
+    select_date(driver)  # choose date
+    click_search_button(driver)  # click search button
+    time.sleep(1)
+    data_array = get_data_array(driver)  # create the array of data
+    time.sleep(1)
+    # write_to_csv(data_array)  # update csv files
     driver.quit()
